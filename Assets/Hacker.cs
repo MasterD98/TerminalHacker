a991c7491ab493c9cc43ef22c563913b248b3e3d
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-01-31 09:29:43 +0530

    Created Enum class called Screen, created method to choose difficulty

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    int Level;
+    enum Screen {
+        MainMenu,
+        Password,
+        Win
+    }
+    Screen CurrentScreen=Screen.MainMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,13 +18,44 @@ public class Hacker : MonoBehaviour
     }
     void ShowMainMenu() {
         Terminal.ClearScreen();
-        string Greeting = "Hello Ben";
-        Terminal.WriteLine(Greeting);
         Terminal.WriteLine("What Would you like to hack into?" +
             " \nPress 1 for local library " +
             "\nPress 2 for the police station" +
             "\nPress 3 for NASA" +
             "\n\nEnter your selection");
+    }
+    void OnUserInput(string Input)
+    {
+        if (Input == "menu"){ShowMainMenu();}
+        else if (CurrentScreen == Screen.MainMenu) { RunMainMenu(Input); }
+    }
+
+    private void RunMainMenu(string Input)
+    {
+        if (Input == "1"){
+            Level = 1;
+            StartGame();
+        }
+        else if (Input == "2"){
+            Level = 2;
+            StartGame();
+        }
+        else if (Input == "3"){
+            Level = 3;
+            StartGame();
+        }
+        else if (Input == "007"){
+            Terminal.WriteLine("Please select a level Mr.Bond");
+        }
+        else{
+            Terminal.WriteLine("Please choose a valid input");
+        }
+    }
+
+    void StartGame() {
+        CurrentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level "+Level);
+        Terminal.WriteLine("Please enter your password");
     }
     // Update is called once per frame
     void Update()
