4ec3c510a91824d05818c119376dfb3e3d069a81
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-01-31 11:38:18 +0530

    created several methods and refactor the code

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
@@ -13,6 +10,8 @@ public class Hacker : MonoBehaviour
     }
     Screen CurrentScreen;
     string Password;
+    string[] Level1Passwords = {"books","aisle","self","password","font","borrow"};
+    string[] Level2Passwords = {"prisoner","handcuffs","holster","uniform","arrest"};
     // Start is called before the first frame update
     void Start()
     {
@@ -37,27 +36,14 @@ public class Hacker : MonoBehaviour
         else if (CurrentScreen == Screen.Password) { CheckPassword(Input); }
     }
 
-    private void CheckPassword(string input)
-    {
-        if (input == Password) {
-            CurrentScreen = Screen.Win;
-            Terminal.WriteLine("Well Done!!"); 
-        }
-        else { 
-            Terminal.WriteLine("Wrong password");
-        }
-    }
+    
+
 
     private void RunMainMenu(string Input)
     {
-        if (Input == "1"){
-            Level = 1;
-            Password = "dog";
-            StartGame();
-        }
-        else if (Input == "2"){
-            Level = 2;
-            Password = "Donkey";
+        bool isValidInput = (Input == "1" || Input == "2");
+        if (isValidInput) {
+            Level = int.Parse(Input);
             StartGame();
         }
         else if (Input == "007"){
@@ -70,9 +56,73 @@ public class Hacker : MonoBehaviour
 
     void StartGame() {
         CurrentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen level "+Level);
+        Terminal.ClearScreen();
+        GenPassword();
         Terminal.WriteLine("Please enter your password");
     }
+
+    void CheckPassword(string input)
+    {
+        if (input == Password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            Terminal.WriteLine("Wrong password");
+        }
+    }
+    void GenPassword(){
+        switch (Level) {
+            case 1:
+                Password = Level1Passwords[Random.Range(0, Level1Passwords.Length)];
+                break;
+            case 2:
+                Password = Level2Passwords[Random.Range(0, Level2Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+    void DisplayWinScreen()
+    {
+        CurrentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        switch (Level) {
+            case 1:
+                Terminal.WriteLine("Have a book");
+                Terminal.WriteLine(@"
+    _______
+   /      /,
+  /      //
+ /______//
+(______(/
+");
+                break;
+            case 2:
+                Terminal.WriteLine("You got the prison key");
+                Terminal.WriteLine(@"
+/<__>\
+\    /
+ '. |
+  < |
+  < |
+  <_/
+
+");
+                break;
+            default:
+                Debug.LogError("Invalid level");
+                break;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
